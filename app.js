2af1b6d9e0cc019c5949acb66d63a40a52093c45
Russell Oyama <russelloyama@gmail.com>
2023-04-05 06:48:34 -1000

    env file added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -17,11 +18,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String 
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
  
